01e3df4acb3b3a482f131dc970334914ff35f077
Alexey <alexeylosev1501@gmail.com>
2022-08-31 20:30:27 +0500

    Initializers and setup methods was added.

--- SettingsScreen/App/ProfileCustomTableViewCell.swift
+++ SettingsScreen/App/ProfileCustomTableViewCell.swift
@@ -8,5 +8,31 @@
 import UIKit
 
 class ProfileCustomTableViewCell: UITableViewCell {
-
+    
+    //MARK: - Outlets
+    
+    
+    //MARK: - Initializers
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Setup
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    //MARK: - Reuse
 }
