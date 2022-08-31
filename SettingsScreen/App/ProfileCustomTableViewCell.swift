e586e63eea6631a1706d1045eaf9110f9d04e6c4
Alexey <alexeylosev1501@gmail.com>
2022-08-31 20:27:44 +0500

    profile cell file was created

---
+++ SettingsScreen/App/ProfileCustomTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileCustomTableViewCell.swift
+//  SettingsScreen
+//
+//  Created by Алексей Лосев on 31.08.2022.
+//
+
+import UIKit
+
+class ProfileCustomTableViewCell: UITableViewCell {
+
+}
