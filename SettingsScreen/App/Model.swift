03f886689f60e78f60e9af74349a892553b37bae
Alexey <alexeylosev1501@gmail.com>
2022-09-01 15:54:07 +0500

    some model changes.

--- SettingsScreen/App/Model.swift
+++ SettingsScreen/App/Model.swift
@@ -8,43 +8,40 @@
 import UIKit
 
 struct ProfileSettingsItems {
-    var personImage: UIImage
-    var name: String
-    var content: String
-}
-
-struct CellItems {
-    var icon: UIImage
-    var title: String
-    var isSwitch: Bool
-}
-
-struct CellItemsWithNotification {
-    var icon: UIImage
-    var title: String
-    var notification: UIImageView
+    var cellImage: UIImage
+    var notificationImage: UIImage?
+    var cellBackgroundColor: UIColor?
+    var cellTitle: String
+    var statusLabel: UILabel?
+    var isSwitchActivate: Bool?
+    var isZeroSection: Bool?
 }
 
 extension ProfileSettingsItems {
-    static var myProfile: ProfileSettingsItems = ProfileSettingsItems(
-        personImage: UIImage(named: "myPhoto") ?? UIImage(systemName: "house")!,
-        name: "Алексей Лосев",
-        content: "Apple ID, iCloud, контент и покупки")
-}
-
-extension CellItems {
-    static var cellItems: [[CellItems]] = [
-        [CellItems(icon: UIImage(systemName: "airplane")!, title: "Авиарежим", isSwitch: true),
-         CellItems(icon: UIImage(systemName: "wifi")!, title: "Wi-Fi", isSwitch: false),
-         CellItems(icon: UIImage(systemName: "airplane")!, title: "Bluetooth", isSwitch: false),
-         CellItems(icon: UIImage(systemName: "airplane")!, title: "Сотовая связь", isSwitch: false),
-         CellItems(icon: UIImage(systemName: "airplane")!, title: "Режим модема", isSwitch: false),
-         CellItems(icon: UIImage(systemName: "airplane")!, title: "VPN", isSwitch: true)],
-        
-        [CellItems(icon: UIImage(systemName: "airplane")!, title: "Уведомления", isSwitch: false),
-         CellItems(icon: UIImage(systemName: "airplane")!, title: "Звуки, тактильные сигналы", isSwitch: false),
-         CellItems(icon: UIImage(systemName: "airplane")!, title: "Не беспокоить", isSwitch: false),
-         CellItems(icon: UIImage(systemName: "airplane")!, title: "Экранное время", isSwitch: false)],
-    ]
     
+    static var settingsCell: [[ProfileSettingsItems]] = [
+        [ProfileSettingsItems(
+            cellImage: UIImage(named: "myPhoto") ?? UIImage(systemName: "house")!,
+            cellTitle: "Алексей Лосев",
+            isZeroSection: true],
+    
+    ]
+//        content: "Apple ID, iCloud, контент и покупки")
 }
+//
+//extension CellItems {
+//    static var cellItems: [[CellItems]] = [
+//        [CellItems(icon: UIImage(systemName: "airplane")!, title: "Авиарежим", isSwitch: true),
+//         CellItems(icon: UIImage(systemName: "wifi")!, title: "Wi-Fi", isSwitch: false),
+//         CellItems(icon: UIImage(systemName: "airplane")!, title: "Bluetooth", isSwitch: false),
+//         CellItems(icon: UIImage(systemName: "airplane")!, title: "Сотовая связь", isSwitch: false),
+//         CellItems(icon: UIImage(systemName: "airplane")!, title: "Режим модема", isSwitch: false),
+//         CellItems(icon: UIImage(systemName: "airplane")!, title: "VPN", isSwitch: true)],
+//
+//        [CellItems(icon: UIImage(systemName: "airplane")!, title: "Уведомления", isSwitch: false),
+//         CellItems(icon: UIImage(systemName: "airplane")!, title: "Звуки, тактильные сигналы", isSwitch: false),
+//         CellItems(icon: UIImage(systemName: "airplane")!, title: "Не беспокоить", isSwitch: false),
+//         CellItems(icon: UIImage(systemName: "airplane")!, title: "Экранное время", isSwitch: false)],
+//    ]
+//
+//}
--- SettingsScreen/App/ViewController.swift
+++ SettingsScreen/App/ViewController.swift
@@ -9,12 +9,51 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-
+    
+   private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(ProfileCustomTableViewCell.self, forCellReuseIdentifier: "ProfileCustomTableViewCell")
+        tableView.dataSource = self
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
+        title = "Настройки"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        setupHierarchy()
+        setupLayout()
     }
-
-
+    
+    private func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupLayout() {
+        tableView.snp.makeConstraints { make in
+            make.top.right.bottom.left.equalTo(view)
+        }
+    }
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCustomTableViewCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
